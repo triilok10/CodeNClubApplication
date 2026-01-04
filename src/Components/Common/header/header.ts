570e69fd5026d841191bceb793090ae057573555
triilok10 <ittiku3@gmail.com>
2026-01-04 12:39:31 +0530

    Mapped the Routing

--- src/Components/Common/header/header.ts
+++ src/Components/Common/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
